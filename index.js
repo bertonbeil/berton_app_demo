9334980ab3798d304d01c09d6d12a0179c241b64
Andrew <skateboardsmart@gmail.com>
2017-10-18 05:08:05 +0300

    connect mongodb, add default requests

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // set up express app
 const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/chargo');
+mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 
 // initialise routes
 app.use('/api', require('./routes/api'));
 
+//error handling
+app.use(function (err, req, res, next) {
+  //console.log(err);
+  res.status(422).send({error: err.message});
+});
+
 // listen for request
 app.listen(process.env.port || 4000, function () {
-  console.log('TA-DA!!!');
+  console.log('ready for requests');
 });
--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const Char = require('../models/char');
 
 // get characters list from the db
-router.get('/chars', function (req, res) {
+router.get('/chars', function (req, res, next) {
   res.send({ type: 'GET' });
 });
 
 // add a new character list to the db
-router.post('/chars', function (req, res) {
-  console.log(req.body);
-  res.send({ type: 'POST' });
+router.post('/chars', function (req, res, next) {
+  Char.create(req.body).then(function (char) {
+    res.send(char);
+  }).catch(next);
+
 });
 
 // update a character in the db
-router.put('/chars/:id', function (req, res) {
-  res.send({ type: 'PUT' });
+router.put('/chars/:id', function (req, res, next) {
+  Char.findByIdAndUpdate({_id: req.params.id}, req.body).then(function () {
+    Char.findOne({_id: req.params.id}).then(function (char) {
+      res.send(char);
+    });
+  });
 });
 
 // delete a characters from the db
-router.delete('/chars/:id', function (req, res) {
-  res.send({ type: 'DELETE' });
+router.delete('/chars/:id', function (req, res, next) {
+  Char.findByIdAndRemove({_id: req.params.id}).then(function (char) {
+    res.send(char);
+  });
 });
 
 module.exports = router;
